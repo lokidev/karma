edc4398594e0ee320f5936115c82beff3ae4e87b
Joshua French <11663405+jfrench7919@users.noreply.github.com>
2021-09-27 00:31:01 -0400

    Updated decorater

--- Api/Decoraters/PersonDecorator.cs
+++ Api/Decoraters/PersonDecorator.cs
@@ -6,19 +6,55 @@ using System.Threading.Tasks;
 
 namespace Karma.Decoraters
 {
-  public class PersonDecorator
-  {
-    public static Person setLuck(Person person)
+    public class PersonDecorator
     {
-      person.Luck = RandomPercentCalculator();
-      return person;
-    }
+        public static Person setBirthDate(Person person)
+        {
+            person.BirthDate = DateTime.Now;
+            return person;
+        }
 
-    public static float RandomPercentCalculator()
-    {
+        public static Person setGender(Person person)
+        {
+            person.Gender = RandomBoolCalculator();
+            return person;
+        }
+
+        public static Person setLuck(Person person)
+        {
+            person.Luck = RandomPercentCalculator();
+            return person;
+        }
+
+        public static Person setHealth(Person person)
+        {
+            person.Health = RandomPercentCalculator();
+            return person;
+        }
+
+        public static Person setHunger(Person person)
+        {
+            person.Hunger = RandomPercentCalculator();
+            return person;
+        }
+
+        public static Person setSecurity(Person person)
+        {
+            person.Security = RandomPercentCalculator();
+            return person;
+        }
+
+        public static float RandomPercentCalculator()
+        {
             Random r = new Random();
             int rInt = r.Next(50, 100); //for ints
             return rInt;
+        }
+
+        public static bool RandomBoolCalculator()
+        {
+            Random r = new Random();
+            return r.Next(2) == 0;
+        }
     }
-  }
 }
